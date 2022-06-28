ac05a19c707722fe1a36fd1b55e1e79206651579
SujalDeep <deepsujal2000@gmail.com>
2022-06-28 14:53:15 +0530

    changes

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static("public"));
 mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true});
 
